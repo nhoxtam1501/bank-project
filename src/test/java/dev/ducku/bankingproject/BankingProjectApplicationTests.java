6c1a2d626e8f3a657c4b290daae6e22adf2a230b
Duc Tam ( ͡• ͜ʖ ͡• ) <huynhductam69@gmail.com>
2024-12-05 11:38:50 +0700

    added mysql configuration into main.yml

--- src/test/java/dev/ducku/bankingproject/BankingProjectApplicationTests.java
+++ src/test/java/dev/ducku/bankingproject/BankingProjectApplicationTests.java
@@ -1,6 +1,7 @@
 package dev.ducku.bankingproject;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
